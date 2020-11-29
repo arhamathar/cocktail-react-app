31dcf280191b780621fdb1376b041e46c67b8248
arhamathar <arhamathar789@gmail.com>
2020-11-30 02:25:41 +0530

    navbar and about page added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="logo">The<span>Cocktail</span></div>
+        <ul className="nav-links">
+          <li>
+            <NavLink activeClassName="selected" to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="selected" to="/about">About</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Error() {
   return (
     <div>
-      <h1>Error page</h1>
+      <h1><span> 404 </span>Page Not Found <span>:{"("}</span></h1>
     </div>
   )
 }
