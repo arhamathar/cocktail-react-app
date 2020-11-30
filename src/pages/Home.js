8340eee38f8d9f913592fcd53d28820543dcd3dc
arhamathar <arhamathar789@gmail.com>
2020-11-30 13:03:36 +0530

    api get reques setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import SingleCocktail from './SingleCocktail';
 
 function Home() {
+  const [loading, setLoading] = useState(false);
+  const [loadedCocktails, setLoadedCocktails] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const sendRequest = async () => {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`
+        );
+        const responseData = await response.json();
+        console.log(responseData.drinks);
+        setLoadedCocktails(responseData.drinks);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+    sendRequest();
+  }, []);
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <main>
+      <SingleCocktail
+        loadedCocktails={loadedCocktails}
+        loading={loading}
+      />
+    </main>
   )
 }
 
